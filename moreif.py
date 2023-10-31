a9ad6a4c459c6b5256be2cc9491dc867ba0f9755
silassakwa <silassakwa2016@gmail.com>
2023-10-31 00:13:32 -0700

    updated print statement

--- moreif.py
+++ moreif.py
@@ -13,4 +13,4 @@ answer=int(input("What is the answer of 5+5: "))
 if answer!=10:
     print("Incorrect 5 + 5 is not equals to {}".format(answer))
 else:
-    print("Congratulation !Correct 5+ 5 =10,")
+    print("Correct 5+ 5 =10,")
