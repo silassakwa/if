409d75cff3c6370b408a05db652e23a19af27e44
silassakwa <silassakwa2016@gmail.com>
2023-10-31 00:42:20 -0700

    numbers which are divisblle by 7

--- odd_even.py
+++ odd_even.py
@@ -1,6 +1,7 @@
 ''''
 a python program to determine if the number is odd or even
 '''
+count=0
 for number in range(1,100):
     '''
     arithmetic operators
@@ -10,7 +11,10 @@ for number in range(1,100):
     musltiplication *
     modulation %
     '''
-    if number%2==0:
-        print("{} is even number".format(number))
+    if number%7==0:
+        print("{} is divisible by 7 ".format(number))
+        count=count+1
     else:
-        print("{} is odd number".format(number))
+        print("{} is not divisible by 7".format(number))
+
+    print("Total numbers divisible by 7 are",count)
