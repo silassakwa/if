0778b769fe9bcb284405dfee56e7ac4663d0f64c
silassakwa <silassakwa2016@gmail.com>
2023-10-31 00:36:37 -0700

    odd_even

---
+++ odd_even.py
@@ -0,0 +1,16 @@
+''''
+a python program to determine if the number is odd or even
+'''
+for number in range(1,100):
+    '''
+    arithmetic operators
+    addituion +
+    substraction -
+    division /
+    musltiplication *
+    modulation %
+    '''
+    if number%2==0:
+        print("{} is even number".format(number))
+    else:
+        print("{} is odd number".format(number))
